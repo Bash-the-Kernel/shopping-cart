993c99ad80bff2dd23cc98d2c627d121799d1fcf
Jack McWilliams <jackcharlie258@gmail.com>
2025-01-28 16:21:34 -0600

    fixed error where cart page didnt load

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import "./App.css";
@@ -10,15 +10,20 @@ const Shop = React.lazy(() => import("./pages/Shop"));
 const Cart = React.lazy(() => import("./pages/Cart"));
 
 const App = () => {
+  const [cart, setCart] = useState([]); // State to manage the cart
+
+  // Calculate the total number of items in the cart
+  const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <HelmetProvider>
       <Router>
         <Suspense fallback={<div>Loading...</div>}>
-          <Navbar />
+          <Navbar cartCount={cartCount} />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/shop" element={<Shop cart={cart} setCart={setCart} />} />
+            <Route path="/cart" element={<Cart cart={cart} />} />
           </Routes>
         </Suspense>
       </Router>
