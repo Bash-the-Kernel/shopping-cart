971d8470e3acafc83beb4ae97f2492249b06738d
Jack McWilliams <jackcharlie258@gmail.com>
2025-01-29 15:12:31 -0600

    adjusted robots.txt in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
       <Helmet>
         <title>Home - Our Shop</title>
         <meta name="description" content="Welcome to Our Shop! Explore the best products at amazing prices." />
+        <meta name="robots" content="index, follow" /> {/* Allow indexing */}
       </Helmet>
       <h1 className="home-title">Welcome to Our Shop!</h1>
       <p>Explore the best products available at amazing prices.</p>
